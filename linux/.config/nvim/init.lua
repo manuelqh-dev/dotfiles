110a0acc6e19717462c57b0c7736d3029186f773
Manuel Quintana Huerta <110722207+manuelqh-dev@users.noreply.github.com>
2025-08-10 10:26:21 +0000

    Update init.lua

--- linux/.config/nvim/init.lua
+++ linux/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- Configuración básica
+vim.opt.rtp:prepend("~/.local/share/nvim/site/pack/lazy/start/lazy.nvim")
 vim.o.termguicolors = true
 vim.o.number = true
 vim.o.relativenumber = true
@@ -13,5 +14,64 @@ vim.o.smartcase = true
 vim.o.hlsearch = true
 vim.o.incsearch = true
 
--- Configuración del tema Midnight Circuit
-require('themes.midnight_circuit').setup()
+require("lazy").setup({
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+        float = {
+          transparent = false,
+          solid = false,
+        },
+        show_end_of_buffer = false,
+        term_colors = false,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        no_italic = false,
+        no_bold = false,
+        no_underline = false,
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        auto_integrations = false,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+      end,
+  },
+})
